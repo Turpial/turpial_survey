bc7c27d68663eb7872b334087a28d27753a703b8
satanas <wil.alejandro@gmail.com>
2012-10-15 15:04:06 -0300

    Added survey text and removed show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 TurpialSurveys::Application.routes.draw do
-  resources :surveys, except: ['destroy', 'update', 'edit']
+  resources :surveys, except: ['destroy', 'update', 'edit', 'show']
   match 'stats' => 'surveys#stats'
   root :to => 'surveys#new'
 end
