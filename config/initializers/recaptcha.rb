2ae4d65bccc27c5656600a20e6a416e82c8b4f3c
satanas <wil.alejandro@gmail.com>
2012-10-15 17:44:13 -0300

    Enabled external config for recaptcha

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,5 +1,6 @@
+keys = YAML.load(File.open(Rails.root.join('config', 'captchakeys.yml')))
 Recaptcha.configure do |config|
-  config.public_key  = ''
-  config.private_key = ''
+  config.public_key  = keys['config']['public']
+  config.private_key = keys['config']['private']
   config.proxy = ''
 end
