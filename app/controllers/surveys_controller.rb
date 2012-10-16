032bc26680dbf3251ca17f4509c1d3c4a3f74245
satanas <wil.alejandro@gmail.com>
2012-10-15 23:15:22 -0300

    Commented out the captcha settings

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -17,7 +17,8 @@ class SurveysController < ApplicationController
     @survey = Survey.new(params[:survey])
     @tweet_url = "https://twitter.com/intent/tweet?original_referer=#{@url}&source=tweetbutton&text=#{CGI.escape(I18n.t('tweet_text'))}&url=#{@url}&via=turpialVe"
     respond_to do |format|
-      if verify_recaptcha(:model => @survey, :message => I18n.t('recaptcha.errors.verification_failed')) && @survey.save
+      #if verify_recaptcha(:model => @survey, :message => I18n.t('recaptcha.errors.verification_failed')) && @survey.save
+      if @survey.save
         format.html
       else
         format.html { render :action => "new" }
